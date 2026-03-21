c22245759526300f07085a69901fdcdb45370484
Rohith <b.rohith5118@gmail.com>
2026-03-21 12:55:17 +0530

    created basic structure

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class foodItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    type = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import foodItem
 
 # Create your views here.
+
+def home(request):
+    items = foodItem.objects.all()
+    return render(request,'home.html',{'items':items})
