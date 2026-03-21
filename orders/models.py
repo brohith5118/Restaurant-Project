7a495f38d094527761387cbf0ca9a893d191a5dd
Rohith <b.rohith5118@gmail.com>
2026-03-21 18:22:53 +0530

    improved ui, added images, added nav bar

--- orders/models.py
+++ orders/models.py
@@ -6,6 +6,7 @@ class foodItem(models.Model):
     price = models.IntegerField()
     type = models.CharField(max_length=50)
     description = models.TextField()
+    image = models.ImageField(upload_to='food_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
--- orders/views.py
+++ orders/views.py
@@ -13,9 +13,10 @@ def add_item(request):
         name = request.POST.get('name')
         type = request.POST.get('type')
         description = request.POST.get('description')
+        image = request.FILES.get('image')
 
-        foodItem.objects.create(name=name,price=price,type=type,description=description)
-        return redirect(request.META.get('HTTP_REFERER', '/'))
+        foodItem.objects.create(name=name,price=price,type=type,description=description,image=image)
+        return redirect('manager_view')
     return render(request,'add_item.html')
 
 def manager_view(request):
@@ -26,7 +27,7 @@ def delete_item(request,id):
     if(request.method == 'POST'):
         item = foodItem.objects.get(id=id)
         item.delete()
-    return redirect(request.META.get('HTTP_REFERER', '/'))
+    return redirect('manager_view')
 
 def open_edit(request,id):
     item = foodItem.objects.get(id=id)
@@ -38,7 +39,9 @@ def edit_item(request, id):
     if request.method == "POST":
         item.name = request.POST.get('name')
         item.price = request.POST.get('price')
+        if request.FILES.get('image'):
+            item.image = request.FILES.get('image')
         item.save()
-        return redirect('/')
+        return redirect('manager_view')
 
     return render(request, 'edit_item.html', {'item': item})
