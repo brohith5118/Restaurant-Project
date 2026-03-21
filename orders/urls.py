3056942efc67ae8213bed7a22fd20bef2ff1d86e
Rohith <b.rohith5118@gmail.com>
2026-03-21 14:08:19 +0530

    added home screen with dispaly food list and manager screen to edit,delete, and add food items

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
+    path('add',views.add_item),
+    path('manager_view',views.manager_view),
+    path('delete/<int:id>',views.delete_item)
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import foodItem
 
 # Create your views here.
@@ -6,3 +6,24 @@ from .models import foodItem
 def home(request):
     items = foodItem.objects.all()
     return render(request,'home.html',{'items':items})
+
+def add_item(request):
+    if(request.method == 'POST'):
+        price = request.POST.get('price')
+        name = request.POST.get('name')
+        type = request.POST.get('type')
+        description = request.POST.get('description')
+
+        foodItem.objects.create(name=name,price=price,type=type,description=description)
+        return redirect('/')
+    return render(request,'add_item.html')
+
+def manager_view(request):
+    items = foodItem.objects.all()
+    return render(request,'manager_view.html',{'items':items})
+
+def delete_item(request,id):
+    if(request.method == 'POST'):
+        item = foodItem.objects.get(id=id)
+        item.delete()
+    return redirect('/')
