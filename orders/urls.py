370afb84bb4213aacd50619124a260727411da83
Rohith <b.rohith5118@gmail.com>
2026-03-21 17:16:58 +0530

    improved ui for the website

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('',views.home),
     path('add',views.add_item),
     path('manager_view',views.manager_view),
-    path('delete/<int:id>',views.delete_item)
+    path('delete/<int:id>',views.delete_item),
+    path('open_edit/edit/<int:id>',views.edit_item),
+    path('open_edit/<int:id>',views.open_edit),
 ]
--- orders/views.py
+++ orders/views.py
@@ -15,7 +15,7 @@ def add_item(request):
         description = request.POST.get('description')
 
         foodItem.objects.create(name=name,price=price,type=type,description=description)
-        return redirect('/')
+        return redirect(request.META.get('HTTP_REFERER', '/'))
     return render(request,'add_item.html')
 
 def manager_view(request):
@@ -26,4 +26,19 @@ def delete_item(request,id):
     if(request.method == 'POST'):
         item = foodItem.objects.get(id=id)
         item.delete()
-    return redirect('/')
+    return redirect(request.META.get('HTTP_REFERER', '/'))
+
+def open_edit(request,id):
+    item = foodItem.objects.get(id=id)
+    return render(request,'edit_item.html',{'item':item})
+
+def edit_item(request, id):
+    item = foodItem.objects.get(id=id)
+
+    if request.method == "POST":
+        item.name = request.POST.get('name')
+        item.price = request.POST.get('price')
+        item.save()
+        return redirect('/')
+
+    return render(request, 'edit_item.html', {'item': item})
